dead57b5b66235d415bffc31b79f38a84778bcdf
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-05-27 22:14:26 +0200

    simplified

--- portfolio-frontend/src/pages/Blog.tsx
+++ portfolio-frontend/src/pages/Blog.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Container from '@mui/material/Container'
-import BlogList from '../components/BlogList'
 import useGetEducation from '../hooks/useGetEducation'
+import { Grid } from '@material-ui/core'
+import BlogCard from '../components/BlogCard'
 
 
 const Blog: React.FC = () => {
-  
+
   // const posts = useGetEducation()
-  
+
   const posts = [
     { id: '1', title: 'First Post', description: 'This is the first post.', image: 'https://via.placeholder.com/600x400' },
     { id: '2', title: 'Second Post', description: 'This is the second post.', image: 'https://via.placeholder.com/600x400' },
@@ -16,7 +17,13 @@ const Blog: React.FC = () => {
 
   return (
     <Container>
-      <BlogList posts={posts} />
+      <Grid container spacing={4}>
+        {posts.map((post) => (
+          <Grid item xs={12} sm={6} md={4} key={post.id}>
+            <BlogCard id={post.id} title={post.title} description={post.description} image={post.image} />
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   )
 }
