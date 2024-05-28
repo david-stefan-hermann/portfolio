c5a673d5a647ff00696c230a350c8b9a079ab794
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-05-28 02:02:36 +0200

    more timelining

--- portfolio-frontend/src/components/BlogCard.tsx
+++ portfolio-frontend/src/components/BlogCard.tsx
@@ -11,19 +11,37 @@ interface BlogCardProps {
 
 const BlogCard: React.FC<BlogCardProps> = ({ id, title, description, image }) => {
   return (
-    <Row className='w-full pt-3 items-center'>
+    <Row className='w-full pt-3 items-center ml-0'>
       <Col sm={3}>
-        <Row className='items-center'>
-          <Col sm={6} className='text-right -mt-1 pr-6 text-xl font-bold'>2013</Col>
-          <Col sm={6} className='h-2 bg-green-300 rounded-l'>
-            <Row className='-mt-1'>
-              <p className='h-4 w-4 bg-black outline outline-8 outline-green-300 rounded-full'></p>
-              <p className='-mt-1'>
-                Feb 2013
-              </p>
+        <Row>
+          <Col sm={12}>
+            <Row className='text-lg'>
+              <div className='w-full bg-green-200 pl-0 flex flex-row items-center'>
+                <div className='h-4 w-4 mx-2 bg-black outline outline-8 outline-amber-400 rounded-full'></div>
+                <div className='pl-4 flex flex-col'>
+                  <span className=''>2013 Jan</span>
+                  <span className=''>2014 Feb</span>
+                </div>
+              </div>
             </Row>
           </Col>
         </Row>
+        {
+          false &&
+          <>
+            <Row className='items-center'>
+              <Col sm={6} className='text-right -mt-1 pr-6 text-xl font-bold'>2013</Col>
+              <Col sm={6} className='h-2 bg-green-300 rounded-l'>
+                <Row className='-mt-1'>
+                  <p className='h-4 w-4 bg-black outline outline-8 outline-green-300 rounded-full'></p>
+                  <p className='-mt-1'>
+                    Feb 2013
+                  </p>
+                </Row>
+              </Col>
+            </Row>
+          </>
+        }
       </Col>
       <Col sm={9} className='bg-white rounded py-3'>
         <Row>
