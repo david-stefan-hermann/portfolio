2140da15db1b120c2b88773087dbadd820a9bdd3
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-05-29 01:42:13 +0200

    added link

--- portfolio-frontend/src/components/TopNavbar.tsx
+++ portfolio-frontend/src/components/TopNavbar.tsx
@@ -15,6 +15,7 @@ const TopNavbar: React.FC = () => {
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/portfolio">Projekte</Nav.Link>
+            <Nav.Link as={Link} to="/erfahrung">Erfahrung</Nav.Link>
             <Nav.Link as={Link} to="/bildung">Bildung</Nav.Link>
           </Nav>
         </Navbar.Collapse>
