f1dd732c75bbc2d7715b6046c73a50348b3d37d0
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-05-30 00:04:47 +0200

    added background image

--- portfolio-frontend/src/pages/Home.tsx
+++ portfolio-frontend/src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { Box, Typography, useTheme } from '@mui/material'
+import useGetSettings from '../hooks/useGetSettings'
 
 const Home: React.FC = () => {
   const theme = useTheme()
+  const settings = useGetSettings()
   const [headerHeight, setHeaderHeight] = useState(0)
 
   useEffect(() => {
@@ -13,23 +15,69 @@ const Home: React.FC = () => {
   }, [])
 
   return (
-    <Box
-      id="hero"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      height='100vh'
-      marginTop={`-${headerHeight}px`}
-      textAlign="center"
-    >
-      <Typography variant="h1">
-        David Hermann
-      </Typography>
-      <Typography variant="h4">
-        Software Developer und Systemadministrator
-      </Typography>
-    </Box>
+    <>
+      <Box
+        id="hero"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        height='100vh'
+        marginTop={`-${headerHeight}px`}
+        textAlign="center"
+      >
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          height='50vh'
+          width='100%'
+          marginBottom={theme.spacing(4)}
+          sx={{
+            backgroundImage: `url(${settings?.images?.[0] || 'https://via.placeholder.com/1920x1080'})`,
+            backgroundSize: "contain",
+            backgroundPosition: "right top",
+            backgroundRepeat: "no-repeat",
+            borderTop: "solid transparent",
+          }}
+        >
+          <Box
+            sx={{
+              backgroundColor: "rgba(255, 255, 255, 0.4)",
+              borderRadius: theme.spacing(1),
+              padding: theme.spacing(2),
+              marginBottom: `-${theme.spacing(4)}`,
+            }}
+          >
+            <Typography
+              variant="h1"
+              sx={{
+                fontSize: {
+                  xs: "3rem",
+                  sm: "3.5rem",
+                  md: "4rem",
+                },
+              }}
+            >
+              {settings?.hero_title || 'John Doe'}
+            </Typography>
+            <Typography
+              variant="h4"
+              sx={{
+                fontSize: {
+                  xs: "1rem",
+                  sm: "1.5rem",
+                  md: "2rem",
+                },
+              }}
+            >
+              {settings?.hero_subtitle || 'Systemadministrator'}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </>
   )
 }
 
